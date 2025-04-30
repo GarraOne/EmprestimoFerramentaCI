9a809e5febdebbb4cf3a75d45bd1c3f065b9e6f9
Periusdv <dvperius@gmail.com>
2025-04-30 11:30:26 -0300

    feat: Criado as classes de teste para modelo

---
+++ src/test/java/modelo/TesteAmigo.java
@@ -0,0 +1,21 @@
+package modelo;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TesteAmigo {
+    
+    public TesteAmigo() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+}
