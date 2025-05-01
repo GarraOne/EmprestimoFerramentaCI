cc41a2a48124d3c5a0162578756708fcf2805d7d
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-05-01 15:55:54 -0300

    Test: TesteAmigo adicionado test de id, nome amigo, telefone

--- src/test/java/modelo/TesteAmigo.java
+++ src/test/java/modelo/TesteAmigo.java
@@ -4,18 +4,59 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TesteAmigo {
-    
-    public TesteAmigo() {
-    }
-    
+
+    Amigo amigo;
+
     @BeforeAll
-    public static void setUpClass() {
+    void inicializacao() {
+        amigo = new Amigo(1, "Joao", "48999");
+
+    }
+    @Test
+    void testGetIdAmigo() {
+        //Define o valor esperado para a operação
+        int retornoEsperado = 1;
+        //Chama a operacao getIDEmprestimo()
+        int retornoFeito = amigo.getIdAmigo();
+        //Premissa verifica se os valores são iguais
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+    @Test
+    void testGetNomeAmigo() {
+        //Define o valor esperado para a operação
+        String retornoEsperado = "Joao";
+        //Chama a operacao getIDEmprestimo()
+        String retornoFeito = amigo.getNomeAmigo();
+        //Premissa verifica se os valores são iguais
+        assertEquals(retornoEsperado, retornoFeito, "");
     }
     
+    @Test
+    void testGetTelefone() {
+        //Define o valor esperado para a operação
+        String retornoEsperado = "48999";
+        //Chama a operacao getIDEmprestimo()
+        String retornoFeito = amigo.getTelefone();
+        //Premissa verifica se os valores são iguais
+        assertEquals(retornoEsperado, retornoFeito, "");
+    }
+
+    @Test
+    void testGetVazio() {
+        String retornoEsperado = "";
+        Amigo amigo = new Amigo();
+        String retornoFeito = amigo.getNomeAmigo();
+        assertEquals(retornoEsperado, retornoFeito, "");
+    }
+
     @AfterAll
-    public static void tearDownClass() {
+    public void finalizacao() {
+        amigo = null;
     }
 
 }
