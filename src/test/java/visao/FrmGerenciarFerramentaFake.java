e021ec2faa77a6bd676329c0a43b0749d9347e9d
GarraOne <garrra.one@gmail.com>
2025-05-26 18:16:25 -0300

    test: implementando o test modificar ferramenta, do gerenciar ferramenta

---
+++ src/test/java/visao/FrmGerenciarFerramentaFake.java
@@ -0,0 +1,48 @@
+package visao;
+
+public class FrmGerenciarFerramentaFake extends FrmGerenciarFerramenta {
+
+    /**
+     * Inicializa os atributos.
+     */
+    public FrmGerenciarFerramentaFake() {
+        super();
+    }
+
+    @Override
+    public void mostrarMensagem(String mensagem) {
+        //Atribui a mensagem ao atributo para ser utilizado nos testes
+        setMensagem(mensagem);
+
+        //Mostra a mensagem
+        System.out.println("Mensagem:" + mensagem);
+    }
+
+    public void inicializarLista() {
+        CarregaListaFerramenta();
+    }
+
+    public void selecionarLinha(int linha) {
+        getJTableAmigos().setRowSelectionInterval(linha, linha);
+    }
+
+    public javax.swing.JTextField getJTFNome() {
+        return super.getJTFNome();  // usa o getter da superclasse
+    }
+
+    public javax.swing.JTextField getJTFCustoFerramenta() {
+        return super.getJTFCustoFerramenta();
+    }
+
+    public javax.swing.JTextField getJTFMarca() {
+        return super.getJTFMarca();
+    }
+
+    public void clicarBotaoModificar() {
+        super.getJBModificar().doClick();
+    }
+
+    public void clicarBotaoApagar() {
+        super.getJBApagar().doClick();
+    }
+}
