0d9fe0be4e46dab3afd9d60391df88ba8b4eb6bd
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-05-29 18:48:06 -0300

    test: Test DAOException testa DAOException

---
+++ src/test/java/dao/DAOExceptionTest.java
@@ -0,0 +1,33 @@
+package dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DAOExceptionTest {
+    
+   @Test
+    void testConstrutorComMensagem() {
+        String msg = "Erro ao acessar o banco de dados";
+        DAOException ex = new DAOException(msg);
+
+        assertEquals(msg, ex.getMessage());
+        assertNull(ex.getCause());
+    }
+
+    @Test
+    void testConstrutorComMensagemECausa() {
+        String msg = "Erro ao salvar dados";
+        Throwable causa = new NullPointerException("Objeto nulo");
+        DAOException ex = new DAOException(msg, causa);
+
+        assertEquals(msg, ex.getMessage());
+        assertEquals(causa, ex.getCause());
+    }
+}
+    
+
