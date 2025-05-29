1ca239e9e270fe2ebbc707c4421716ec350d60ff
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-05-29 19:03:56 -0300

    refactor: arumando DAOException info

--- src/test/java/dao/DAOExceptionTest.java
+++ src/test/java/dao/DAOExceptionTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.TestInstance;
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DAOExceptionTest {
     
    @Test
