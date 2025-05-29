5c063c9f9e02df244dbfbcea7c94e4d31b1c92fd
Periusdv <dvperius@gmail.com>
2025-05-29 19:42:19 -0300

    refactor: Modificando ConexaoDAO

--- src/main/java/dao/ConexaoDAO.java
+++ src/main/java/dao/ConexaoDAO.java
@@ -3,8 +3,12 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexaoDAO {
+
+    private static final Logger logger = Logger.getLogger(ConexaoDAO.class.getName());
 
     /**
      * Estabelece uma conexão com o banco de dados de empréstimos.
@@ -14,25 +18,24 @@ public class ConexaoDAO {
     public Connection getConexao() {
         Connection connection = null;
         try {
-            // Carrega o driver do banco de dados
-            Class.forName("org.sqlite.JDBC");
-
-            // Estabelece a conexão com o banco de dados
-            connection = DriverManager.getConnection("jdbc:sqlite:emprestimo.db");
-
-            // Verifica se a conexão foi bem-sucedida e exibe uma mensagem
-            System.out.println("Status: Conectado!");
+            carregarDriver();
+            connection = criarConexao();
+            logger.info("Status: Conectado!");
             return connection;
-
         } catch (ClassNotFoundException erro) {
-            // Trata o erro caso o driver não seja encontrado
-            System.out.println("O driver não foi encontrado. " + erro.getMessage());
+            logger.log(Level.SEVERE, "O driver não foi encontrado." + erro);
             return null;
         } catch (SQLException erro) {
-            // Trata o erro caso a conexão com o banco de dados falhe
-            System.out.println("Não foi possível conectar...");
+            logger.log(Level.SEVERE, "Não foi possível conectar ao banco de dados.", erro);
             return null;
         }
     }
 
+    protected void carregarDriver() throws ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+    }
+
+    protected Connection criarConexao() throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlite:emprestimo.db");
+    }
 }
